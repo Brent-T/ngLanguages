b825997e5d6eb8f8236be62f7454db537ef47838
Brent Timmermans <brent.timmermans@hotmail.com>
2017-05-14 16:25:39 +0200

    Stupid dictionairy keys issue fixed

--- src/translate/translations.ts
+++ src/translate/translations.ts
@@ -11,11 +11,10 @@ import { LANG_FR_NAME, LANG_FR_TRANS } from './lang-fr';
 export const TRANSLATIONS = new OpaqueToken('translations');
 
 // all traslations
-const dictionary = {
-	[LANG_EN_NAME]: LANG_EN_TRANS,
-	[LANG_NL_NAME]: LANG_NL_TRANS,
-	[LANG_FR_NAME]: LANG_FR_TRANS,
-};
+export const dictionary = { };
+dictionary[LANG_EN_NAME] = LANG_EN_TRANS;
+dictionary[LANG_NL_NAME] = LANG_NL_TRANS;
+dictionary[LANG_FR_NAME] = LANG_FR_TRANS;
 
 // providers
 export const TRANSLATION_PROVIDERS = [
